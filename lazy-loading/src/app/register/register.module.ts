6410c4851b60ddce65621946f83c9b6807ec670e
Programmer <Programmer@example.com>
2023-02-02 18:17:24 +0530

    first commit

---
+++ lazy-loading/src/app/register/register.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegisterRoutingModule } from './register-routing.module';
+import { RegisterComponent } from './register.component';
+import {ButtonModule} from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    RegisterRoutingModule,
+    ButtonModule,
+    FormsModule
+  ]
+})
+export class RegisterModule { }
