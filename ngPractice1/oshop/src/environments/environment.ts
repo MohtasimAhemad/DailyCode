b92596cf603986ba4942b1a2350d367295e55b8c
Programmer <Programmer@example.com>
2023-03-31 17:33:33 +0530

    initial commint

---
+++ ngPractice1/oshop/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:true,
+    firebase:{
+        apiKey: "AIzaSyAlZ5x8cGRRX2io5cpZK5YiDN78lQMRBzI",
+    authDomain: "oshop-e1c1d.firebaseapp.com",
+    projectId: "oshop-e1c1d",
+    storageBucket: "oshop-e1c1d.appspot.com",
+    messagingSenderId: "211255985140",
+    appId: "1:211255985140:web:bda4b5c90da8e2cf876fa1",
+    measurementId: "G-QK6J6WYDE3"
+    }
+};
